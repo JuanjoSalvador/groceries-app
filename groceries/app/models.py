f2bd6259fe4ad84439107f8cac26fccbce722fc5
Juanjo Salvador <juanjosalvador@netc.eu>
2024-01-21 14:32:01 +0000

    (models) Add Price model, improve Product model

--- groceries/app/models.py
+++ groceries/app/models.py
@@ -3,7 +3,11 @@ from django.db import models
 class Store(models.Model):
     name = models.CharField(max_length=10)
 
+class Price(models.Model):
+    date = models.DateField()
+    value = models.FloatField()
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+
 class Product(models.Model):
     name = models.CharField(max_length=80)
-    price = models.FloatField()
-    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    price = models.ForeignKey(Price, on_delete=models.CASCADE)
