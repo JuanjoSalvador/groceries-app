12be3c144e44ccc00ded8460c9e63d5817c6ec11
Juanjo Salvador <juanjosalvador@netc.eu>
2024-01-22 15:28:10 +0000

    (views) Add search view, fix product view

--- groceries/app/views.py
+++ groceries/app/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
+from .models import Product
+
+
 def index(request):
     return render(request, "index.html")
 
+def search(request):
+    query = request.POST['searchbar']
+    queryset = Product.objects.filter(name__contains=query)
+
+    values = {"search": query, "queryset": queryset}
+
+    return render(request, "search.html", values)
+
 def product_view(request, pk=None):
-    return render(request, "product.html")
+    queryset = Product.objects.get(pk=pk)
+    context = {"product": queryset}
+
+    return render(request, "product.html", context)
