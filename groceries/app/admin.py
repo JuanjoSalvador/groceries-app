bcac2a898ad88a92d0a7ec825c81891e7b514df3
Juanjo Salvador <juanjosalvador@netc.eu>
2024-01-27 21:48:03 +0000

    (models) Add ProductHistory object, remove Price object

--- groceries/app/admin.py
+++ groceries/app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Price, Product, Store
+from .models import Product, ProductHistory, Store
 
-admin.site.register(Price)
 admin.site.register(Product)
 admin.site.register(Store)
+
+admin.site.register(ProductHistory)
--- groceries/app/models.py
+++ groceries/app/models.py
@@ -1,22 +1,40 @@
+from datetime import date
+
 from django.db import models
 
 class Store(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=20)
 
     def __str__(self, *args, **kwargs):
         return f"{self.name}"
-
-class Price(models.Model):
-    date = models.DateField()
-    value = models.FloatField()
-    store = models.ForeignKey(Store, on_delete=models.CASCADE)
-
-    def __str__(self, *args, **kwargs):
-        return f"{self.value}€/unidad en {self.store.name}"
 
 class Product(models.Model):
     name = models.CharField(max_length=80)
-    price = models.ForeignKey(Price, on_delete=models.CASCADE)
+    brand = models.CharField(max_length=80, default="")
+    description = models.CharField(max_length=500, default="")
+    price = models.FloatField()
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, null=True)
 
     def __str__(self, *args, **kwargs):
-        return f"{self.name}"
+        return f"{self.name} - {self.brand}"
+    
+    def save(self, *args, **kwargs):
+        super(Product, self).save(*args, **kwargs)
+
+        ProductHistory.objects.create(
+            product = self,
+            price = self.price,
+            date = date.today()
+        )
+
+class ProductHistory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = price = models.FloatField()
+    date = models.DateField()
+
+    def __str__(self, *args, **kwargs):
+        return f"{self.product.name} - {self.product.brand} ({self.date})"
+
+    class Meta:
+        verbose_name = "Price history"
+        verbose_name_plural = "Price history"
